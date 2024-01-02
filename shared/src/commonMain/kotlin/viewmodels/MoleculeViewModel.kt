2bfe648cb0ec7736df4db23ee0d72559da5e2df5
Richard Woollcott <richard.woollcott@appmilla.co.uk>
2024-01-02 22:17:27 +0000

    Tidyup remove unused stuff

--- shared/src/commonMain/kotlin/viewmodels/MoleculeViewModel.kt
+++ shared/src/commonMain/kotlin/viewmodels/MoleculeViewModel.kt
@@ -2,6 +2,10 @@ package viewmodels
 
 import kotlinx.coroutines.CoroutineScope
 
+/**
+ * Base class for Molecule ViewModels based on KaMPKit.
+ * The molecule scope is used to setup the MonotonicFrameClock for each platform
+ */
 expect open class MoleculeViewModel(scope: CoroutineScope? = null) {
     val viewModelScope: CoroutineScope
     val moleculeScope: CoroutineScope
